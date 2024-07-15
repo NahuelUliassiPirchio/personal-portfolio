fefb44ab2717399374c062974639382dfc6fd380
Nahuel Uliassi Pirchio <uliassipichio@gmail.com>
2024-07-15 19:06:40 -0300

    [Add] Image Spinner

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -9,78 +9,98 @@ import styles from '../styles/ImageSlider.module.css'
 export default function ImageSlider ({ images }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const [showModal, setShowModal] = useState(false)
+  const [loading, setLoading] = useState(images.map(() => true))
 
   const applySlideScroll = () => {
     const previewsContainer = document.querySelector(`.${styles.previewsContainer}`)
     const preview = document.querySelector(`.${styles.preview}`)
-
-    previewsContainer.scrollTo({
-      left: preview.offsetWidth * currentImageIndex,
-      behavior: 'smooth'
-    })
+    if (previewsContainer && preview) {
+      previewsContainer.scrollTo({
+        left: preview.offsetWidth * currentImageIndex,
+        behavior: 'smooth'
+      })
+    }
   }
+
   const handleNextImage = () => {
-    applySlideScroll()
     setCurrentImageIndex((currentImageIndex + 1) % images.length)
-  }
-  const handlePrevImage = () => {
     applySlideScroll()
-    setCurrentImageIndex(
-      currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1
-    )
   }
 
-  const handleModalClick = (e) => {
+  const handlePrevImage = () => {
+    setCurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1)
+    applySlideScroll()
+  }
+
+  const handleModalClick = () => {
     setShowModal(false)
   }
 
+  const handleImageLoad = (index) => {
+    setLoading((prevLoading) => {
+      const newLoading = [...prevLoading]
+      newLoading[index] = false
+      return newLoading
+    })
+  }
+
   return (
-      <div className={styles.container}>
-        <ul className={styles.previewsContainer}>
-          {images.map((imageUrl, index) => (
-            <li key={imageUrl} className={styles.preview}>
-              <Image
-                className={`${styles.preview} ${currentImageIndex === index ? styles.active : null}`}
-                src={imageUrl}
-                alt="preview"
-                width={50}
-                height={50}
-                onClick={() => setCurrentImageIndex(index)}
-              />
-            </li>
-          ))}
-        </ul>
+    <div className={styles.container}>
+      <ul className={styles.previewsContainer}>
+        {images.map((imageUrl, index) => (
+          <li key={imageUrl} className={styles.preview}>
+            <Image
+              className={`${styles.preview} ${currentImageIndex === index ? styles.active : ''}`}
+              src={imageUrl}
+              alt="preview"
+              width={50}
+              height={50}
+              onClick={() => setCurrentImageIndex(index)}
+              onLoad={() => handleImageLoad(index)}
+            />
+          </li>
+        ))}
+      </ul>
 
-        <figure>
-          <button className={`${styles.navigationButton} ${styles.prevButton}`} onClick={handlePrevImage}>
-            <Image src={sortLeftIcon} alt="prev" width={20} height={20} />
-          </button>
-          {
-            images.map((imageUrl, index) => {
-              return (
-                <Image className={`${styles.image} ${index === currentImageIndex && styles.display}`}
-                  key={index}
-                  src={imageUrl}
-                  onClick={() => setShowModal(true)}
-                  alt="slider"
-                  width={800}
-                  height={400} />
-              )
-            })
-          }
-          <button className={`${styles.navigationButton} ${styles.nextButton}`} onClick={handleNextImage}>
-            <Image src={sortRightIcon} alt="next" width={20} height={20} />
-          </button>
-        </figure>
+      <figure>
+        <button className={`${styles.navigationButton} ${styles.prevButton}`} onClick={handlePrevImage}>
+          <Image src={sortLeftIcon} alt="prev" width={20} height={20} />
+        </button>
+        <div className={styles.imageContainer}>
+          {loading[currentImageIndex] && (
+            <div className={styles.loader}>
+              <div className={styles.spinner}></div>
+            </div>
+          )}
+          <Image
+            className={`${styles.image} ${!loading[currentImageIndex] && styles.display}`}
+            src={images[currentImageIndex]}
+            onClick={() => setShowModal(true)}
+            alt="slider"
+            width={800}
+            height={400}
+            onLoad={() => handleImageLoad(currentImageIndex)}
+          />
+        </div>
+        <button className={`${styles.navigationButton} ${styles.nextButton}`} onClick={handleNextImage}>
+          <Image src={sortRightIcon} alt="next" width={20} height={20} />
+        </button>
+      </figure>
 
-        {showModal && (
-          <div className={styles.modal} onClick={handleModalClick}>
-            <Image src={images[currentImageIndex]} alt="modal" width={800} height={800} />
-          </div>
-        )}
-      </div>
+      {showModal && (
+        <div className={styles.modal} onClick={handleModalClick}>
+          <Image
+            src={images[currentImageIndex]}
+            alt="modal"
+            width={800}
+            height={800}
+            onLoad={() => handleImageLoad(currentImageIndex)}
+          />
+        </div>
+      )}
+    </div>
   )
-};
+}
 
 ImageSlider.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired
