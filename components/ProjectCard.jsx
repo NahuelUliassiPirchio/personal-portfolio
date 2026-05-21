cc842cca57cb694eaebef13a772767fc50d20b33
NahuelUliassiPirchio <uliassipirchio@gmail.com>
2026-05-21 04:03:57 -0300

    [Update] Refactor LogoLink and ProjectCard components to remove logo prop and enhance styling; update favicon and CSS for improved layout

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,9 +5,6 @@ import Image from 'next/image'
 import LogoLink from './LogoLink'
 import LinksDropDown from './LinksDropDown'
 import { getLinkByName } from '../utils/projectFilters'
-import githubLogo from '../public/icons/github.svg'
-
-import rightArrowIcon from '../public/icons/right-arrow.svg'
 import styles from '../styles/ProjectsCard.module.css'
 
 export default function ProjectCard ({ project, t }) {
@@ -62,26 +59,29 @@ export default function ProjectCard ({ project, t }) {
         </div>
 
         <div className={styles.projectButtons}>
-          {
-            projectRepoLinks.length > 1
-              ? (
-                <LinksDropDown links={projectRepoLinks}>
-                  <LogoLink text='Repos' logo={githubLogo} href={projectRepoLinks[0].url}/>
-                </LinksDropDown>
-                )
-              : (
-                <LogoLink text='Repo' logo={githubLogo} href={projectRepoLinks[0].url}/>
-                )
-          }
-          {/* {projectDemoLink && <Link className={styles.demoButton} href={projectDemoLink.url} passHref target={'_blank'}>Live Demo</Link>} */}
-          {projectDemoLink && <Link className={styles.demoButton} href={projectDemoLink.url} passHref target={'_blank'}>
-                    {t('visitDemo')}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"/></svg>
-                  </Link>}
-          <Link className={styles.readMoreLink} href={`/projects/${project.url}`}>{
-            t('viewProject')}
-            <Image className={styles.arrow} src={rightArrowIcon} alt='Right arrow' width={30} height={30} />
-            </Link>
+          <div className={styles.projectButtonsLeft}>
+            {
+              projectRepoLinks.length > 1
+                ? (
+                  <LinksDropDown links={projectRepoLinks}>
+                    <LogoLink text='Repos' href={projectRepoLinks[0].url}/>
+                  </LinksDropDown>
+                  )
+                : (
+                  <LogoLink text='Repo' href={projectRepoLinks[0].url}/>
+                  )
+            }
+            {projectDemoLink && <Link className={styles.demoButton} href={projectDemoLink.url} passHref target={'_blank'}>
+                      {t('visitDemo')}
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"/></svg>
+                    </Link>}
+          </div>
+          <Link className={styles.readMoreLink} href={`/projects/${project.url}`}>
+            {t('viewProject')}
+            <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrow} viewBox="0 0 24 24" width="20" height="20" fill="currentColor" aria-hidden="true">
+              <path d="M8.59 16.59L10 18l6-6-6-6-1.41 1.41L12.17 11H4v2h8.17l-1.58 1.59z"/>
+            </svg>
+          </Link>
         </div>
       </div>
     </li>
